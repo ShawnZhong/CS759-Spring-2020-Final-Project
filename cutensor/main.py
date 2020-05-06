32387dec784913c3ed7418b795ec4d89acd978d7
Shawn Zhong <github@shawnzhong.com>
2020-05-06 03:07:15 -0500

    Update main.py

--- cutensor/main.py
+++ cutensor/main.py
@@ -5,30 +5,37 @@ import torch
 import os
 from pathlib import Path
 import matplotlib.pyplot as plt
+from cupy.cuda import stream
 
 # 'abcd,aefd->aefbc'
 
-def einsum_cutensor(n):
-    a = cupy.random.rand(n, n, n, n)
-    b = cupy.random.rand(n, n, n, n)
+batch_dim = 200
 
-    arr_out = cupy.empty([n, n, n, n, n], cupy.float64)
+def einsum_cutensor(n):
+    st = stream.Stream()
+    a = cupy.random.rand(batch_dim, n, n, n)
+    b = cupy.random.rand(batch_dim, n, n, n)
+
+    arr_out = cupy.empty([batch_dim, n, n, n, n])
     arr0 = cupy.ascontiguousarray(a)
     arr1 = cupy.ascontiguousarray(b)
     desc_0 = cutensor.create_tensor_descriptor(arr0)
     desc_1 = cutensor.create_tensor_descriptor(arr1)
     desc_out = cutensor.create_tensor_descriptor(arr_out)
-    arr_out = cutensor.contraction(1.0,
-                                arr0, desc_0, list(ord(c) for c in "abcd"),
-                                arr1, desc_1, list(ord(c) for c in "aefd"),
-                                0.0,
-                                arr_out, desc_out, list(ord(c) for c in "aefbc"))
+    with st:
+        arr_out = cutensor.contraction(1.0,
+                                    arr0, desc_0, list(ord(c) for c in "abcd"),
+                                    arr1, desc_1, list(ord(c) for c in "aefd"),
+                                    0.0,
+                                    arr_out, desc_out, list(ord(c) for c in "aefbc"))
+    st.synchronize()
 
 
 def einsum_pytorch(n):
-    a = torch.rand(n, n, n, n)
-    b = torch.rand(n, n, n, n)
+    a = torch.rand(batch_dim, n, n, n)
+    b = torch.rand(batch_dim, n, n, n)
     torch.einsum("abcd,aefd->aefbc", a, b)
+    torch.cuda.synchronize()
 
 def benchmark(func, n):
     for _ in range(3):
@@ -44,7 +51,7 @@ def main():
     output_dir = Path(__file__).parent
     os.makedirs(output_dir, exist_ok=True)
 
-    xs = range(2, 49, 2)
+    xs = range(2, 31, 2)
 
     for func in (einsum_cutensor, einsum_pytorch):
         times = [benchmark(func, n) for n in xs]
